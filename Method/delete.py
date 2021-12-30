3c87c055ca97b9e5f5094d312226188462d1687b
Leo Amato <lamato@liricus.com.ar>
2021-12-30 15:10:57 -0300

    code clean (delete)

--- Method/delete.py
+++ Method/delete.py
@@ -9,7 +9,6 @@ from Utils.console import clear_console
 def delete_appointment(client, CustomerKey, User, Password):
     AppointmentId = input("Enter the appointment id: ")
     term_size = os.get_terminal_size()
-    print("\n")
     print('\033[33m'+'=' * term_size.columns + '\033[0m')
 
     try:
@@ -17,50 +16,47 @@ def delete_appointment(client, CustomerKey, User, Password):
         print("\n"+"Creating factory: DeleteAppointmentReq ...", end="", flush=True)
         DeleteAppointmentReq = client.factory.create('DeleteAppointmentReq')
         print('\033[32m', "(Ok)", '\033[0m')
-        print("\n", DeleteAppointmentReq)
+
         print('\033[33m'+'=' * term_size.columns + '\033[0m')
 
         #   Assign the values
-        print("\n"+"Assigning the values to: RequestHeader ...", end="", flush=True)
-        DeleteAppointmentReq.RequestHeader = {
-            "CustomerKey" : CustomerKey,
-            "Password" : Password,
-            "User" : User
-        }
-        print('\033[32m', "(Ok)", '\033[0m')
         print("\n"+"Assigning the values to: Appointment ...", end="", flush=True)
-        DeleteAppointmentReq.Appointment = {
-            "AppointmentId" : AppointmentId
-        }
+        DeleteAppointmentReq.RequestHeader['CustomerKey'] = CustomerKey
+        DeleteAppointmentReq.RequestHeader['Password'] = Password
+        DeleteAppointmentReq.RequestHeader['User'] = User
+        DeleteAppointmentReq.Appointment['AppointmentId'] = AppointmentId
         print('\033[32m', "(Ok)", '\033[0m')
 
-        #   Factory ready for use
-        print("\n", DeleteAppointmentReq)
         print('\033[33m'+'=' * term_size.columns + '\033[0m')
 
         #   Call the service DeleteAppointment with the param DeleteAppointmentReq
         print("\n"+"Starting request: DeleteAppointment ...", end="", flush=True)
-        response = client.service.DeleteAppointment(DeleteAppointmentReq)
-        print('\033[32m', "(Ok)", '\033[0m')
-        print("\n", response)
+        try:
+            response = client.service.DeleteAppointment(DeleteAppointmentReq)
+            if (response['ErrorResponse']['IsError']):
+                print('\033[31m', "(Fail)", '\033[0m')
+                print("\n\033[31mError:\033[0m", response['ErrorResponse']['ErrorMessage'])
+                print("\n\033[35mStackTrace:\033[0m\n", response['ErrorResponse']['StackTrace'])
+            else:
+                print('\033[32m', "(Ok)", '\033[0m')
+        except Exception as error:
+            return error
+
         print('\033[33m'+'=' * term_size.columns + '\033[0m')
 
         #   Get result of the response
         print("\n"+"Getting result of the response ...", end="", flush=True)
         result = recursive_dict(response)
+        print('\033[32m', "(Ok)", '\033[0m')
         if result['ErrorResponse']['IsError'] == "True":
-            print('\033[31m', "(Fail)", '\033[0m')
-            print("\n\033[31mError:\033[0m", result['ErrorResponse']['ErrorMessage'])
-            print("\n\033[35mStackTrace:\033[0m\n", result['ErrorResponse']['StackTrace'])
+
             result['ErrorResponse']['ErrorMessage'] = "Not exist appointment with id " + AppointmentId
             result['ErrorResponse']['StackTrace'] = ""
             result['AppointmentId'] = AppointmentId
         else:
-            print('\033[32m', "(Ok)", '\033[0m')
-        result = json.dumps(result, indent=4)
+            result = json.dumps(result, indent=4)
 
-        print("\nResult:", result)
-
+        print("\n", result)
         return result
     except Exception as error:
         return error
